720e2fdcf2df91b51c94b0826e022490cd4d65a9
trongthang197 <55197021+trongthang197@users.noreply.github.com>
2019-10-07 15:23:31 +0700

    Update Search

--- FPT-Libol-master/Libol/OPAC/Controllers/HomeController.cs
+++ FPT-Libol-master/Libol/OPAC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OPAC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,29 @@ namespace OPAC.Controllers
         // GET: Home
         public ActionResult Home()
         {
-            return View();
+            var model = new OptionModel
+            {
+                Option = OptionList()
+            };
+
+            return View(model);
+        }
+
+        private List<SelectListItem> OptionList()
+        {
+            List<SelectListItem> items = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Mọi trường", Value = "0" },
+                new SelectListItem { Text = "Nhan đề", Value = "1" },
+                new SelectListItem { Text = "Tác giả", Value = "2" },
+                new SelectListItem { Text = "Nhà xuất bản", Value = "3" },
+                new SelectListItem { Text = "Chỉ số DDC", Value = "4" },
+                new SelectListItem { Text = "Ngôn ngữ", Value = "5" },
+                new SelectListItem { Text = "Từ khóa", Value = "6" },
+                new SelectListItem { Text = "Môn học", Value = "7" }
+            };
+
+            return items;
         }
     }
 }
